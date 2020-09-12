001494a61c9181b1f5fd2921ef119cca8fc2be6f
Fakhri Aslan <aslan.ov@outlook.com>
2020-09-12 12:25:51 +0300

    new nav implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,15 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+
+    "use strict";
+
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+
+};
