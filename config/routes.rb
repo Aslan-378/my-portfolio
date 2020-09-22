9dab740f40b54db597d87ea60b2adf3d7805ecc0
Fakhri Aslan <aslan.ov@outlook.com>
2020-09-22 13:52:35 +0300

    added contact form and tested it locally

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'pages/contents'
   get 'pages/cv'
+
+  resources :contacts, only: [:new, :create]
 end
